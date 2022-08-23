77a00abf1abe75bd380a348d7cf8a1653cd4c31d
karishmask46 <karishmask.15999@gmail.com>
2022-08-23 21:24:18 +0530

    Anagram Detection

--- Day16_17algorithmProgram/Day16_17algorithmProgram/Program.cs
+++ Day16_17algorithmProgram/Day16_17algorithmProgram/Program.cs
@@ -7,7 +7,7 @@ class Program
         while (flag == "Y" || flag == "y")
         {
             Console.WriteLine("Welcome To Datastructures and Algorithms");
-            Console.WriteLine("1.Permutation Using Recursion\n2.BinarySearch\n3.InsertionSort\n4.BubbleSort\n5.MergeSort");
+            Console.WriteLine("1.Permutation Using Recursion\n2.BinarySearch\n3.InsertionSort\n4.BubbleSort\n5.MergeSort\n6.Anagram");
             Console.WriteLine("Enter Your Option");
             int num = Convert.ToInt32(Console.ReadLine());
             switch (num)
@@ -28,9 +28,13 @@ class Program
                 //    BubbleSort bubblesort = new BubbleSort();
                 //    bubblesort.bubbleSort();
                 //    break;
-                case 5:
-                    MergeSort mergesort = new MergeSort();
-                    mergesort.mergeSort();
+                //case 5:
+                //    MergeSort mergesort = new MergeSort();
+                //    mergesort.mergeSort();
+                //    break;
+                case 6:
+                    Anagrams anagrams = new Anagrams();
+                    anagrams.checkAnagrams();
                     break;
                 default:
                     Console.WriteLine("-------Enter the valid option---");
