731576e4dc9bd5ca230b0929d90f4848c4ed2c2c
karishmask46 <karishmask.15999@gmail.com>
2022-08-23 22:15:05 +0530

    FindNumber

--- Day16_17algorithmProgram/Day16_17algorithmProgram/Program.cs
+++ Day16_17algorithmProgram/Day16_17algorithmProgram/Program.cs
@@ -7,7 +7,7 @@ class Program
         while (flag == "Y" || flag == "y")
         {
             Console.WriteLine("Welcome To Datastructures and Algorithms");
-            Console.WriteLine("1.Permutation Using Recursion\n2.BinarySearch\n3.InsertionSort\n4.BubbleSort\n5.MergeSort\n6.Anagram\n7.PrimeNumber\n8.AnagramAndPalindromeNumber\n9.SearchingAndSortingUsingGenerics ");
+            Console.WriteLine("1.Permutation Using Recursion\n2.BinarySearch\n3.InsertionSort\n4.BubbleSort\n5.MergeSort\n6.Anagram\n7.PrimeNumber\n8.AnagramAndPalindromeNumber\n9.SearchingAndSortingUsingGenerics\n10.FindNumber ");
             Console.WriteLine("Enter Your Option");
             int num = Convert.ToInt32(Console.ReadLine());
             switch (num)
@@ -44,9 +44,13 @@ class Program
                 //    AnagramAndPalindromeNumber anagramandpalindromenumber = new AnagramAndPalindromeNumber();
                 //    anagramandpalindromenumber.anagramAndPalindrome();
                 //    break;
-                case 9:
-                    SearchingAndSortingUsingGenerics searchingandsortingusinggenerics = new SearchingAndSortingUsingGenerics();
-                    searchingandsortingusinggenerics.SearchingAndSorting();
+                //case 9:
+                //    SearchingAndSortingUsingGenerics searchingandsortingusinggenerics = new SearchingAndSortingUsingGenerics();
+                //    searchingandsortingusinggenerics.SearchingAndSorting();
+                //    break;
+                case 10:
+                    FindNumber findnumber = new FindNumber();
+                    findnumber.Find();
                     break;
                 default:
                     Console.WriteLine("-------Enter the valid option---");
