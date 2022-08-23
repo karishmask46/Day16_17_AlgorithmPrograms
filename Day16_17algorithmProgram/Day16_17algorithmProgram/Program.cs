f84e392c9c27e7c70ea39ce068345641065eab91
karishmask46 <karishmask.15999@gmail.com>
2022-08-23 23:20:35 +0530

    TaskDoneByMaximumAmount

--- Day16_17algorithmProgram/Day16_17algorithmProgram/Program.cs
+++ Day16_17algorithmProgram/Day16_17algorithmProgram/Program.cs
@@ -7,7 +7,7 @@ class Program
         while (flag == "Y" || flag == "y")
         {
             Console.WriteLine("Welcome To Datastructures and Algorithms");
-            Console.WriteLine("1.Permutation Using Recursion\n2.BinarySearch\n3.InsertionSort\n4.BubbleSort\n5.MergeSort\n6.Anagram\n7.PrimeNumber\n8.AnagramAndPalindromeNumber\n9.SearchingAndSortingUsingGenerics\n10.FindNumber\n11.TaskDoneByMaximumAmount  ");
+            Console.WriteLine("1.Permutation Using Recursion\n2.BinarySearch\n3.InsertionSort\n4.BubbleSort\n5.MergeSort\n6.Anagram\n7.PrimeNumber\n8.AnagramAndPalindromeNumber\n9.SearchingAndSortingUsingGenerics\n10.FindNumber\n11.TaskDoneByMaximumAmount\n12.CustomisedMessages  ");
             Console.WriteLine("Enter Your Option");
             int num = Convert.ToInt32(Console.ReadLine());
             switch (num)
@@ -52,9 +52,13 @@ class Program
                 //    FindNumber findnumber = new FindNumber();
                 //    findnumber.Find();
                 //    break;
-                case 11:
-                    TaskDoneByMaximumAmount taskdonebymaximumamount = new TaskDoneByMaximumAmount();
-                    taskdonebymaximumamount.tasks();
+                //case 11:
+                //    TaskDoneByMaximumAmount taskdonebymaximumamount = new TaskDoneByMaximumAmount();
+                //    taskdonebymaximumamount.tasks();
+                //    break;
+                case 12:
+                    CustomizedMessage customizedmessage = new CustomizedMessage();
+                    customizedmessage.printMessage();
                     break;
                 default:
                     Console.WriteLine("-------Enter the valid option---");
--- Day16_17algorithmProgram/Day16_17algorithmProgram/TaskDoneByMaximumAmount.cs
+++ Day16_17algorithmProgram/Day16_17algorithmProgram/TaskDoneByMaximumAmount.cs
@@ -2,76 +2,67 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Day16_17algorithmProgram
 {
-    class TaskDoneByMaximumAmount
+    class CustomizedMessage
     {
-        public void tasks()
+        public static string Regex_name = "^[A-Z][a-z]{2,}$";
+        public static string Regex_MobileNumber = "^([0-9]{2}[ -]{1})?[6-9][0-9]{9}$";
+        public static string Regex_Date = "^(0?[1-9]|[12][0-9]|3[01])[/.-]{1}(0?[1-9]|1[012])[/.-]{1}([1-2][0-9]{3})$";
+
+        class Person
         {
-            Console.Write("Number of Tasks:   ");
-            int T = Convert.ToInt32(Console.ReadLine());
+            //default values
+            public string firstName = "abc";
+            public string lastName = "xyz";
+            public string mobile = "91-XXXXXXXXXX";
+            public string date = "01/01/2016";
 
-            List<List<int>> taskList = new List<List<int>>();
+        }
+        public void printMessage()
+        {
 
-            for (int i = 0; i < T; i++)
-            {
-                int M, D;
-                List<int> inputs = new List<int>();
-                Console.Write("\nTask-{0}\nDeadline:  ", i + 1);
-                D = Convert.ToInt32(Console.ReadLine());
-                inputs.Add(D);
+            Person user = new Person();
+            Console.WriteLine("Please Enter User details:  ");
+            Console.Write("First Name:  ");
+            string fName = Console.ReadLine();
+            Console.Write("Last Name:  ");
+            string lName = Console.ReadLine();
+            Console.Write("Mobile Number:  ");
+            string mNo = Console.ReadLine();
+            Console.Write("Date:  ");
+            string dd = Console.ReadLine();
 
-                Console.Write("Time Taken: ");
-                M = Convert.ToInt32(Console.ReadLine());
-                inputs.Add(M);
-
-                taskList.Add(inputs);
-            }
-            Console.WriteLine("\n\n");
-
-            List<List<int>> result = TaskDone(taskList);
-            for (int i = 0; i < result.Count; i++)
-            {
-                Console.WriteLine("Task -{0} overshoots its deadline {1} by {2}\n", i + 1, result[i][0], result[i][2]);
-            }
+            string fullName = checkRegex(user, fName, lName, mNo, dd);
+            Console.Clear();
+            string str = "Hello " + user.firstName + ", \n\nWe have your full name as " + fullName + " in our system. Your contact number is " + user.mobile + ". \nPlease let us know in case of any clarification. \n\nThank you \n\n\nBridgeLabz \n" + user.date + ".";
+            Console.WriteLine(str);
         }
 
-        List<List<int>> TaskDone(List<List<int>> taskList)
+
+        string checkRegex(Person user, string fName, string lName, string mNo, string dd)
         {
-
-            int first = 0;
-            int sum = 0;
-            bool[] time = new bool[1000000];
-            for (int i = 0; i < taskList.Count; i++)
+            string fullName = user.firstName + " " + user.lastName;     // default full name = abc xyz
+            if (Regex.IsMatch(fName, Regex_name) && Regex.IsMatch(lName, Regex_name))
             {
-                int deadline = taskList[i][0];
-                int minutes = taskList[i][1];
-
-                for (int j = deadline - 1; j >= first && minutes > 0; j--)
-                {
-                    if (!time[j])
-                    {
-                        time[j] = true;
-                        minutes--;
-                    }
-                }
-
-                sum = sum + minutes;
-                taskList[i].Add(sum);
-
-                for (int k = first; k < 1000000; k++)
-                {
-                    if (time[k] == false)
-                    {
-                        first = k;
-                        break;
-                    }
-                }
+                user.firstName = fName;
+                user.lastName = lName;
+                fullName = user.firstName + " " + user.lastName;
             }
-            return taskList;
+
+            if (Regex.IsMatch(mNo, Regex_MobileNumber))
+            {
+                user.mobile = mNo;
+            }
+
+            if (Regex.IsMatch(dd, Regex_Date))
+            {
+                user.date = dd;
+            }
+            return fullName;
         }
     }
-
 }
