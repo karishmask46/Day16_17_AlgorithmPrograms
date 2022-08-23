bd89ba0cd930c3307a4a053d2e9dbb0904cd8fa5
karishmask46 <karishmask.15999@gmail.com>
2022-08-23 21:33:58 +0530

    AnagramAndPalindromeNumber

--- Day16_17algorithmProgram/Day16_17algorithmProgram/Program.cs
+++ Day16_17algorithmProgram/Day16_17algorithmProgram/Program.cs
@@ -7,7 +7,7 @@ class Program
         while (flag == "Y" || flag == "y")
         {
             Console.WriteLine("Welcome To Datastructures and Algorithms");
-            Console.WriteLine("1.Permutation Using Recursion\n2.BinarySearch\n3.InsertionSort\n4.BubbleSort\n5.MergeSort\n6.Anagram\n7.PrimeNumber");
+            Console.WriteLine("1.Permutation Using Recursion\n2.BinarySearch\n3.InsertionSort\n4.BubbleSort\n5.MergeSort\n6.Anagram\n7.PrimeNumber\n8.AnagramAndPalindromeNumber ");
             Console.WriteLine("Enter Your Option");
             int num = Convert.ToInt32(Console.ReadLine());
             switch (num)
@@ -36,9 +36,13 @@ class Program
                 //    Anagrams anagrams = new Anagrams();
                 //    anagrams.checkAnagrams();
                 //    break;
-                case 7:
-                    PrimeNumbers primenumbers = new PrimeNumbers();
-                    primenumbers.primeNumbers();
+                //case 7:
+                //    PrimeNumbers primenumbers = new PrimeNumbers();
+                //    primenumbers.primeNumbers();
+                //    break;
+                case 8:
+                    AnagramAndPalindromeNumber anagramandpalindromenumber = new AnagramAndPalindromeNumber();
+                    anagramandpalindromenumber.anagramAndPalindrome();
                     break;
                 default:
                     Console.WriteLine("-------Enter the valid option---");
