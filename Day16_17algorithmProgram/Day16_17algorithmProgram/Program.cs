36c21cb973adbc36facdcc5f07ac715a0905724a
karishmask46 <karishmask.15999@gmail.com>
2022-08-23 14:13:57 +0530

    Permutation of String

---
+++ Day16_17algorithmProgram/Day16_17algorithmProgram/Program.cs
@@ -0,0 +1,29 @@
+﻿using Day16_17algorithmProgram;
+class Program
+{
+    static void Main(String[] args)
+    {
+        string flag = "Y";
+        while (flag == "Y" || flag == "y")
+        {
+            Console.WriteLine("Welcome To Datastructures and Algorithms");
+            Console.WriteLine("1.Permutation Using Recursion");
+            Console.WriteLine("Enter Your Option");
+            int num = Convert.ToInt32(Console.ReadLine());
+            switch (num)
+            {
+                case 1:
+                    PermutationsOfString permutationsofstring = new PermutationsOfString();
+                    permutationsofstring.permutations();
+                    break;
+
+                default:
+                    Console.WriteLine("-------Enter the valid option---");
+                    break;
+            }
+            Console.WriteLine("\nDo you want to continue");
+            flag = Console.ReadLine();
+        }
+        Console.ReadKey();
+    }
+}
