de0dd359db98f0eeefaf3b4b10a89c0782c95c73
karishmask46 <karishmask.15999@gmail.com>
2022-08-23 23:13:21 +0530

    TaskDoneByMaximumAmount

--- Day16_17algorithmProgram/Day16_17algorithmProgram/Program.cs
+++ Day16_17algorithmProgram/Day16_17algorithmProgram/Program.cs
@@ -7,7 +7,7 @@ class Program
         while (flag == "Y" || flag == "y")
         {
             Console.WriteLine("Welcome To Datastructures and Algorithms");
-            Console.WriteLine("1.Permutation Using Recursion\n2.BinarySearch\n3.InsertionSort\n4.BubbleSort\n5.MergeSort\n6.Anagram\n7.PrimeNumber\n8.AnagramAndPalindromeNumber\n9.SearchingAndSortingUsingGenerics\n10.FindNumber ");
+            Console.WriteLine("1.Permutation Using Recursion\n2.BinarySearch\n3.InsertionSort\n4.BubbleSort\n5.MergeSort\n6.Anagram\n7.PrimeNumber\n8.AnagramAndPalindromeNumber\n9.SearchingAndSortingUsingGenerics\n10.FindNumber\n11.TaskDoneByMaximumAmount  ");
             Console.WriteLine("Enter Your Option");
             int num = Convert.ToInt32(Console.ReadLine());
             switch (num)
@@ -48,9 +48,13 @@ class Program
                 //    SearchingAndSortingUsingGenerics searchingandsortingusinggenerics = new SearchingAndSortingUsingGenerics();
                 //    searchingandsortingusinggenerics.SearchingAndSorting();
                 //    break;
-                case 10:
-                    FindNumber findnumber = new FindNumber();
-                    findnumber.Find();
+                //case 10:
+                //    FindNumber findnumber = new FindNumber();
+                //    findnumber.Find();
+                //    break;
+                case 11:
+                    TaskDoneByMaximumAmount taskdonebymaximumamount = new TaskDoneByMaximumAmount();
+                    taskdonebymaximumamount.tasks();
                     break;
                 default:
                     Console.WriteLine("-------Enter the valid option---");
